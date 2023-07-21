7e0cabffde8052f53c81559b3ce5cf0dca73373c
Eric K <54124297+Nyveon@users.noreply.github.com>
2023-07-21 06:56:08 -0400

    Agrega cache de descriptores offline

--- Codigo/busqueda_descr.py
+++ Codigo/busqueda_descr.py
@@ -2,9 +2,16 @@ import numpy as np
 import time
 import os
 import json
+import glob
+import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
 
+# Config
 global umbral
 umbral = 0.01
+descriptors_file = "descriptors.pkl"
+script_path = os.path.abspath(__file__)
+script_dir = os.path.dirname(script_path)
 
 '''
 NOTA: Se intentaron metodos como LSA y cdist pero
@@ -25,6 +32,61 @@ def calcular_descriptores(vectorizer, texto, show=True):
         print("Tiempo descriptores: {:.1f} segs".format(t1-t0), end='\n\n')
 
     return descriptores
+
+
+def calcular_descriptores_local(show=True):
+    '''
+    Funcion para calcular descriptores
+    de los videos del profesor
+    '''
+    vectorizer = TfidfVectorizer(
+        lowercase=True,
+        strip_accents='unicode',
+        sublinear_tf=True,
+        norm='l2',
+        ngram_range=(1, 1),  # Probar distintos valores
+        max_df=1.0,
+        # Si una palabra aparece en más que max_df documentos, se ignora
+        # Si es float -> porcentaje del total de documentos
+        # Si es int   -> cantidad de documentos
+        min_df=1   # Si aparece en menos, se ignora, misma idea de int y float
+    )
+
+    # Cambiar el directorio de trabajo al directorio de los textos completos
+    script_path = os.path.abspath(__file__)
+    script_dir = os.path.dirname(script_path)
+    previous_path = os.path.dirname(script_dir)
+
+    texts_path = (
+        f'{previous_path}/Videos/Transcripciones/Transcripcion_completa'
+    )
+
+    txt_files = glob.glob(texts_path + "/*.txt")
+
+    textos = []
+    nombres_completos = []
+    nombres = []
+    for file_ in txt_files:
+        nombres.append(file_.split('\\')[-1].split('.txt')[0])
+        with open(file_, 'r', encoding="utf-8") as file:
+            nombres_completos.append(file.readline().strip())
+            file.readline()
+            textos.append(file.readline())
+
+    # Calcular el vocabulario
+    t0 = time.time()
+    vectorizer.fit(textos)
+    t1 = time.time()
+    print(f'Tiempo para calcular el vocab: {t1-t0}')
+
+    # Calculando descriptores
+    t0 = time.time()
+    descriptores = vectorizer.transform(textos)
+    t1 = time.time()
+    if show:
+        print("Tiempo descriptores: {:.1f} segs".format(t1-t0), end='\n\n')
+
+    return nombres, descriptores, vectorizer
 
 
 def b_multiplicacion_matrices(nombres, descriptores, textos_consulta,
@@ -91,21 +153,32 @@ def obtener_indices_n_mayores(valores, nombres, n=10):
     return [nombres[i] for i in indices_mayores]
 
 
+def buscar(textos_consulta: list, n: int, recalc=False) -> dict:
+    """Busca los n videos más similares a cada consulta
+
+    Args:
+        texto_consulta (list): lista de queries
+        n (int): numero de resultados por query
+
+    Returns:
+        dict: {query: [video_id1, video_id2, ...]}
+    """
+
+    # Carga los descriptores locales si ya existen o los calcula y guarda.
+    if os.path.exists(f"{script_dir}/descriptors.pkl") and not recalc:
+        print("Cargando descriptores pre-calculados...")
+        with open(f"{script_dir}/descriptors.pkl", "rb") as f:
+            nombres, descriptores, vectorizer = pickle.load(f)  # nosec
+    else:
+        print("Calculando descriptores...")
+        nombres, descriptores, vectorizer = calcular_descriptores_local()
+        with open(f"{script_dir}/descriptors.pkl", "wb") as f:
+            pickle.dump((nombres, descriptores, vectorizer), f)
+
+    return main(descriptores, nombres, vectorizer, textos_consulta, n)
+
+
 def main(descriptores, nombres, vectorizer, textos_consulta, n):
-    """
-    vectorizer = TfidfVectorizer(
-    lowercase = True,
-    strip_accents = 'unicode',
-    sublinear_tf = True,
-    norm = 'l2',
-    ngram_range = (1,1), # Probar distintos valores
-    max_df = 1.0,
-    # Si una palabra aparece en más que max_df documentos, se ignora
-    #  Si es float -> porcentaje del total de documentos
-    #  Si es int   -> cantidad de documentos
-    min_df = 1   # Si aparece en menos, se ignora, misma idea de int y float
-    )
-    """
     # Cambiar el directorio de trabajo al directorio de los textos completos
     script_path = os.path.abspath(__file__)
     script_dir = os.path.dirname(script_path)
--- Codigo/test_ground.py
+++ Codigo/test_ground.py
@@ -1,9 +1,6 @@
 import os
 import pandas as pd
 import busqueda_descr as bus_desc
-from sklearn.feature_extraction.text import TfidfVectorizer
-import glob
-import time
 
 
 def r_prec(ground_values: list[str], search_values: list[str], n=3):
@@ -25,77 +22,18 @@ def mrr(r_precisions: list[int]):
     return sum(r_precisions)/len(r_precisions)
 
 
-def calcular_descriptores_local(show=True):
-    '''
-    Funcion para calcular descriptores
-    de los videos del profesor
-    '''
-    vectorizer = TfidfVectorizer(
-        lowercase=True,
-        strip_accents='unicode',
-        sublinear_tf=True,
-        norm='l2',
-        ngram_range=(1, 1),  # Probar distintos valores
-        max_df=1.0,
-        # Si una palabra aparece en más que max_df documentos, se ignora
-        # Si es float -> porcentaje del total de documentos
-        # Si es int   -> cantidad de documentos
-        min_df=1   # Si aparece en menos, se ignora, misma idea de int y float
-    )
-
-    # Cambiar el directorio de trabajo al directorio de los textos completos
-    script_path = os.path.abspath(__file__)
-    script_dir = os.path.dirname(script_path)
-    previous_path = os.path.dirname(script_dir)
-
-    texts_path = (
-        f'{previous_path}/Videos/Transcripciones/Transcripcion_completa'
-    )
-
-    txt_files = glob.glob(texts_path + "/*.txt")
-
-    textos = []
-    nombres_completos = []
-    nombres = []
-    for file_ in txt_files:
-        nombres.append(file_.split('\\')[-1].split('.txt')[0])
-        with open(file_, 'r', encoding="utf-8") as file:
-            nombres_completos.append(file.readline().strip())
-            file.readline()
-            textos.append(file.readline())
-
-    # Calcular el vocabulario
-    t0 = time.time()
-    vectorizer.fit(textos)
-    t1 = time.time()
-    print(f'Tiempo para calcular el vocab: {t1-t0}')
-
-    # Calculando descriptores
-    t0 = time.time()
-    descriptores = vectorizer.transform(textos)
-    t1 = time.time()
-    if show:
-        print("Tiempo descriptores: {:.1f} segs".format(t1-t0), end='\n\n')
-
-    return nombres, descriptores, vectorizer
-
-
-def calcular_mrr(g_truth, descriptores_locales=None,
-                 nombres=None, vectorizer=None, n=10,
-                 show_individual=False):
+def calcular_mrr(g_truth, n=10, show_individual=False):
     '''
     Calcula el Mean Reciprocal Rank
     g_truth : matris ground
     descriptores_lcoales : descriptores de los videos del profesor
-    nombres: Nombres de los videos del profesor
     vectorizer : vectorizer para calcular descriptores de las consultas
     n : cantidad de valores n que retorno del bus_desc
     '''
     textos_consulta = g_truth['Query'].tolist()
 
     # Comparamos con busqueda_descr
-    result_busc_desc = bus_desc.main(descriptores_locales, nombres,
-                                     vectorizer, textos_consulta, n)
+    result_busc_desc = bus_desc.buscar(textos_consulta, n)
 
     r_precisions = []
     # Algunas métricas
@@ -122,9 +60,6 @@ def main():
     g_truth = pd.read_csv(f"{script_dir}/ground_truth.csv",
                           encoding='utf-8', delimiter=';', dtype=str)
 
-    # Calculamos off-line los descriptores de los videos del profesor
-    nombres, descriptores_locales, vectorizer = calcular_descriptores_local()
-
     # Eliminamos los espacios en blanco del dataframe XD (gracias ChatGPT)
     # Iterar a través de todas las columnas del DataFrame
     for columna in g_truth.columns:
@@ -133,10 +68,8 @@ def main():
             # Eliminar los espacios en blanco de los strings
             g_truth[columna] = g_truth[columna].str.strip()
 
-    mrr_10 = calcular_mrr(g_truth, descriptores_locales,
-                          nombres, vectorizer, n=10)
-    mrr_20 = calcular_mrr(g_truth, descriptores_locales,
-                          nombres, vectorizer, n=20)
+    mrr_10 = calcular_mrr(g_truth, n=10)
+    mrr_20 = calcular_mrr(g_truth, n=20)
 
     print(f'MRR para n-10 : {mrr_10}')
     print(f'MRR para n-20 : {mrr_20}')
