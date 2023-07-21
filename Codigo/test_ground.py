e2fdf1ce59a16620961e1c2eaa5fb01eaca80e5f
Eric K <54124297+Nyveon@users.noreply.github.com>
2023-07-21 13:56:00 -0400

    Agrega mean average precision

--- Codigo/test_ground.py
+++ Codigo/test_ground.py
@@ -41,7 +41,7 @@ def calcular_mrr(g_truth: pd.DataFrame, n: int, buscador: callable,
     textos_consulta = g_truth['Query'].tolist()
 
     # Comparamos con busqueda_descr
-    result_busc_desc = buscador(textos_consulta, n, descriptor, True)
+    result_busc_desc = buscador(textos_consulta, n, descriptor)
 
     r_precisions = []
     # Algunas métricas
@@ -60,6 +60,76 @@ def calcular_mrr(g_truth: pd.DataFrame, n: int, buscador: callable,
     return mrr(r_precisions)
 
 
+def average_precision(ground_values: list[str], search_values: list[str]):
+    '''
+    Calculate the average precision of search_values
+    '''
+    scores = 0
+    num_hits = 0
+    for i, value in enumerate(search_values):
+        if value in ground_values:
+            num_hits += 1
+            scores += num_hits / (i + 1)
+    return scores / min(len(ground_values), len(search_values))
+
+
+def m_ap(average_precisions: list[float]):
+    '''
+    Calculate the mean of the average precisions
+    '''
+    return sum(average_precisions) / len(average_precisions)
+
+
+def calcular_map(g_truth: pd.DataFrame, n: int, buscador: callable,
+                 descriptor: callable, show_individual=False) -> list:
+    """Calculate the Mean Average Precision
+
+    Args:
+        g_truth (pd.DataFrame): Ground truth matrix
+        n (int): Number of results to return
+        buscador (callable): Search function
+        descriptor (callable): Descriptor calculation function
+        show_individual (bool, optional): Show MAP for each query.
+
+    Returns:
+        list: List of MAPs
+    """
+
+    textos_consulta = g_truth['Query'].tolist()
+
+    # Compare with busqueda_descr
+    result_busc_desc = buscador(textos_consulta, n, descriptor)
+
+    average_precisions = []
+    # Some metrics
+    for key in result_busc_desc.keys():
+        resultados = result_busc_desc[key]
+
+        fila_filtrada = g_truth[g_truth['Query'] == key]
+        ground_t = fila_filtrada.iloc[0].tolist()
+
+        avg_prec = average_precision(ground_t, resultados)
+        average_precisions.append(avg_prec)
+
+        if show_individual:
+            print(f'Average Precision for "{key}": {avg_prec:.2f}')
+
+    return m_ap(average_precisions)
+
+
+def bateria_test(g_truth, modulo_buscador, f_descriptor):
+    print((f"\nTesteando buscador {modulo_buscador.__name__}"
+           f" usando {f_descriptor.__name__}"))
+    mrr10 = calcular_mrr(g_truth, 10, modulo_buscador.buscar, f_descriptor)
+    mrr20 = calcular_mrr(g_truth, 20, modulo_buscador.buscar, f_descriptor)
+    map10 = calcular_map(g_truth, 10, modulo_buscador.buscar, f_descriptor)
+    map20 = calcular_map(g_truth, 20, modulo_buscador.buscar, f_descriptor)
+    print(f'MRR para n-10 : {mrr10}')
+    print(f'MRR para n-20 : {mrr20}')
+    print(f'MAP para n-10 : {map10}')
+    print(f'MAP para n-20 : {map20}')
+
+
 def main():
     script_path = os.path.abspath(__file__)
     script_dir = os.path.dirname(script_path)
@@ -76,29 +146,10 @@ def main():
             # Eliminar los espacios en blanco de los strings
             g_truth[columna] = g_truth[columna].str.strip()
 
-    print("Testeando TF-IDF/Multiplicación de Matrices con texto completo:")
-    mrr_10 = calcular_mrr(g_truth, 10, bd.buscar, bd.descriptores_textos)
-    mrr_20 = calcular_mrr(g_truth, 20, bd.buscar, bd.descriptores_textos)
-    print(f'MRR para n-10 : {mrr_10}')
-    print(f'MRR para n-20 : {mrr_20}')
-
-    print("\nTesteando TF-IDF/Multiplicación de Matrices con titulos:")
-    mrr_10 = calcular_mrr(g_truth, 10, bd.buscar, bd.descriptores_titulos)
-    mrr_20 = calcular_mrr(g_truth, 20, bd.buscar, bd.descriptores_titulos)
-    print(f'MRR para n-10 : {mrr_10}')
-    print(f'MRR para n-20 : {mrr_20}')
-
-    print("\nTesteando Fasttext con texto completo:")
-    mrr_10 = calcular_mrr(g_truth, 10, bf.buscar, bf.text_descriptor)
-    mrr_20 = calcular_mrr(g_truth, 20, bf.buscar, bf.text_descriptor)
-    print(f'MRR para n-10 : {mrr_10}')
-    print(f'MRR para n-20 : {mrr_20}')
-
-    print("\nTesteando Fasttext con titulos:")
-    mrr_10 = calcular_mrr(g_truth, 10, bf.buscar, bf.title_descriptor)
-    mrr_20 = calcular_mrr(g_truth, 20, bf.buscar, bf.title_descriptor)
-    print(f'MRR para n-10 : {mrr_10}')
-    print(f'MRR para n-20 : {mrr_20}')
+    bateria_test(g_truth, bd, bd.descriptores_textos)
+    bateria_test(g_truth, bd, bd.descriptores_titulos)
+    bateria_test(g_truth, bf, bf.text_descriptor)
+    bateria_test(g_truth, bf, bf.title_descriptor)
 
 
 if __name__ == '__main__':
